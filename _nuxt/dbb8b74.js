c49d5760dfdaf199832e4698666319cb3a90d8e8
ameer <xerxes1718@gmail.com>
2024-05-04 18:16:20 +0330

    Initial dist subtree commit

---
+++ _nuxt/dbb8b74.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{527:function(n,t,e){"use strict";e.r(t);var l={layout:"dashboard"},o=e(26),component=Object(o.a)(l,(function(){return(0,this._self._c)("div",[this._v("\n  hei\n")])}),[],!1,null,null,null);t.default=component.exports}}]);

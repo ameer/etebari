4e9f8766a85be0bc9740e1f6e16426baac808a6d
Ameer Mousavi <xerxes1718@gmail.com>
2024-05-30 11:48:42 +0330

    test

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      Landing will be here...
+      Landing will be here....
     </v-col>
   </v-row>
 </template>
